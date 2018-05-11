a1859d11bb3a5a64de4135978658351e8983f20b
Ricardo <ricardo.krieg@gmail.com>
2018-05-10 22:01:08 -0300

    phone/email

---
+++ README.md
@@ -0,0 +1,14 @@
+# Running
+
+## Run the Sinatra server
+
+```shell
+$ rackup -p4567
+```
+
+## Run the template for reference
+
+```shell
+$ cd doc/template/biznex/
+$ python -m SimpleHTTPServer
+```
--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require_relative '../config/settings'
 require_relative 'helpers/menu'
 
 before(%r{/(.+)/}) { |path| redirect(path, 301) }
+
+before do
+  @isaveu_phone = settings.phone
+  @isaveu_email = settings.email
+end
 
 get '/' do
   erb :home
