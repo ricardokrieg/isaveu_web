c7f3c2fcba165e9bdb69d29b7b327bce08d70a9e
Ricardo <ricardo.krieg@gmail.com>
2023-05-11 23:19:24 -0300

    change helper

--- app/helpers/menu.rb
+++ app/helpers/menu.rb
@@ -1,29 +1,27 @@
-module MenuHelper
-  class << self
-    def menu(items, current_url)
-      content = ''
+helpers do
+  def menu(items, current_url)
+    content = ''
 
-      items.each do |item|
-        content << menu_item(item[:label], item[:url], current_url)
-      end
-
-      content
+    items.each do |item|
+      content << menu_item(item[:label], item[:url], current_url)
     end
 
-    def menu_item(label, url, current_url)
-      is_current_url = url == current_url
+    content
+  end
 
-      li_class = 'nav-item'
-      li_class += ' active' if is_current_url
+  def menu_item(label, url, current_url)
+    is_current_url = url == current_url
 
-      link_content = label
-      link_content = "<span class='sr-only'>(atual)</span>#{link_content}" if is_current_url
+    li_class = 'nav-item'
+    li_class += ' active' if is_current_url
 
-      content = "<li class='#{li_class}'>"
-      content += "<a class='nav-link' href='#{url}'>#{link_content}</a>"
-      content += "</li>"
+    link_content = label
+    link_content = "<span class='sr-only'>(atual)</span>#{link_content}" if is_current_url
 
-      content
-    end
+    content = "<li class='#{li_class}'>"
+    content += "<a class='nav-link' href='#{url}'>#{link_content}</a>"
+    content += "</li>"
+
+    content
   end
 end
